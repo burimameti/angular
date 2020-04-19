7080b1721068e0575dae79b6fc10a1a5edb6c694
Burim Ameti <Burim.Ameti@inspire11.com>
2020-04-19 14:03:48 +0200

    'all'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {AppService} from ''
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AvatarHoverComponent } from './components/avatar-hover/avatar-hover.component';
+import { ActiveUserComponent } from './components/active-user/active-user.component';
+import { NewOrderFormComponent } from './components/new-order-form/new-order-form.component';
+import { IronHttpInterceptor } from './services/iron/iron-http-interceptor';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data-service/in-memory-data-service.service';
+import { OrderListComponent } from './components/order-list/order-list.component';
+import { SelectUserComponent } from './components/select-user/select-user.component';
+import { TeamManagementToggleComponent } from './components/team-management-toggle/team-management-toggle.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
+import { SelectedUserComponent } from './components/selected-user/selected-user/selected-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AvatarHoverComponent,
+    ActiveUserComponent,
+    NewOrderFormComponent,
+    OrderListComponent,
+    SelectUserComponent,
+    TeamManagementToggleComponent,
+    SnackbarComponent,
+    SelectedUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // Import HttpClientModule after BrowserModule
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {
+      dataEncapsulation: false,
+      passThruUnknownUrl: true,
+      put204: false // return entity after PUT/update
+    }
+    )
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: IronHttpInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
